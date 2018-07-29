98cd16ba8ee250bffcfded9236933d163682e5e7
LaamGinghong <351390560@qq.com>
2018-07-29 14:34:34 +0800

    删除测试文件

--- src/app/pages/content/content.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ContentPipe } from './content.pipe';
-
-describe('ContentPipe', () => {
-  it('create an instance', () => {
-    const pipe = new ContentPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
