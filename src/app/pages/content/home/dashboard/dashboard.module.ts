2d116c3f47a74b945b33ec54b4ae8ff21d7b5e49
LaamGinghong <351390560@qq.com>
2018-08-05 17:15:39 +0800

    初始化dashboard页面首页样式

--- src/app/pages/content/home/dashboard/dashboard.module.ts
+++ src/app/pages/content/home/dashboard/dashboard.module.ts
@@ -1,12 +1,14 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {DashboardComponent} from './dashboard.component';
-import {DashboardRoutingModule} from './dashboard-routing.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { DashboardComponent } from './dashboard.component';
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { NgZorroAntdModule } from '../../../../../../node_modules/ng-zorro-antd';
 
 @NgModule({
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    NgZorroAntdModule
   ],
   declarations: [DashboardComponent]
 })
