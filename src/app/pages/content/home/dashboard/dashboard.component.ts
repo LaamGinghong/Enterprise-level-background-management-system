e78f5526628f4a051b8457be029ebfa18c91dfcd
LaamGinghong <351390560@qq.com>
2018-08-05 23:15:25 +0800

    dashboard卡片样式

--- src/app/pages/content/home/dashboard/dashboard.component.ts
+++ src/app/pages/content/home/dashboard/dashboard.component.ts
@@ -7,10 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+  cardArray = [];
+
   constructor(
   ) { }
 
   ngOnInit() {
+    this.initCardArray();
   }
 
+  initCardArray() {
+    for (let i = 0; i < 10; i++) {
+      const date = new Date();
+      date.setTime(date.getTime() + i * 24 * 60 * 60 * 1000);
+      const option = {
+        number: Math.floor(Math.random() * 10),
+        date: date.toLocaleDateString()
+      };
+      this.cardArray.push(option);
+    }
+    console.log(this.cardArray);
+  }
 }
