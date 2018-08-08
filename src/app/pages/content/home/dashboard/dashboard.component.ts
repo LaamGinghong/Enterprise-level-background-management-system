ae59229cf35c40d592c2be583ccf7dccc52d30fa
LaamGinghong <lanjinghang@banggood.com>
2018-08-08 16:16:57 +0800

    完成中间柱状图、折线图

--- src/app/pages/content/home/dashboard/dashboard.component.ts
+++ src/app/pages/content/home/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,14 +6,94 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-
   cardArray = [];
+  barAbscissa = []; //柱状图横坐标
+  barOrdinate = []; //柱状图纵坐标
+  lineAbscissa = []; //折线图横坐标
+  lineOrdinate1 = []; //折线图纵坐标1
+  lineOrdinate2 = []; //折线图纵坐标2
+  barChart = {
+    xAxis: [
+      {
+        type: 'category',
+        data: this.barAbscissa,
+        axisTick: {
+          alignWithLabel: true
+        }
+      }
+    ],
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'shadow'
+      }
+    },
+    legend: {
+      data: ['文章数量'],
+      top: '2%'
+    },
+    yAxis: [
+      {
+        type: 'value',
+      }
+    ],
+    series: [{
+      name: '文章数量',
+      type: 'bar',
+      data: this.barOrdinate,
+      barWidth: '60%'
+    }],
+    color: ['#58AFFF']
+  };
+  lineChart = {
+    xAxis: [
+      {
+        type: 'category',
+        data: this.lineAbscissa,
+        axisTick: {
+          alignWithLabel: true
+        }
+      }
+    ],
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'line'
+      }
+    },
+    legend: {
+      data: ['客流量', '支付笔数'],
+      top: '2%'
+    },
+    yAxis: [
+      {
+        type: 'value'
+      }
+    ],
+    series: [{
+      name: '客流量',
+      data: this.lineOrdinate1,
+      type: 'line'
+    }, {
+      name: '支付笔数',
+      data: this.lineOrdinate2,
+      type: 'line'
+    }],
+    dataZoom: [
+      {
+        filterMode: 'filter',
+        start: 40,
+        end: 70
+      }]
+  };
 
-  constructor(
-  ) { }
+  constructor() {
+  }
 
   ngOnInit() {
     this.initCardArray();
+    this.initBar();
+    this.initLine();
   }
 
   initCardArray() {
@@ -26,6 +106,20 @@ export class DashboardComponent implements OnInit {
       };
       this.cardArray.push(option);
     }
-    console.log(this.cardArray);
+  }
+
+  initBar() {
+    for (let i = 1; i < 13; i++) {
+      this.barAbscissa.push(`${i}月`);
+      this.barOrdinate.push(Math.floor(Math.random() * 1000));
+    }
+  }
+
+  initLine() {
+    for (let i = 0; i < 24; i++) {
+      this.lineAbscissa.push(i < 10 ? `0${i}:00` : `${i}:00`);
+      this.lineOrdinate1.push(Math.floor(Math.random() * 10));
+      this.lineOrdinate2.push(Math.floor(Math.random() * 10));
+    }
   }
 }
