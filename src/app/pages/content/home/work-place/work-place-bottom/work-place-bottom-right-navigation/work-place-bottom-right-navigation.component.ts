6692534845059a107054495c828d035aec413764
LaamGinghong <lanjinghang@banggood.com>
2018-08-29 10:15:57 +0800

    完成快速开始/便捷导航组件

--- src/app/pages/content/home/work-place/work-place-bottom/work-place-bottom-right-navigation/work-place-bottom-right-navigation.component.ts
+++ src/app/pages/content/home/work-place/work-place-bottom/work-place-bottom-right-navigation/work-place-bottom-right-navigation.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-work-place-bottom-right-navigation',
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./work-place-bottom-right-navigation.component.scss']
 })
 export class WorkPlaceBottomRightNavigationComponent implements OnInit {
+  actionBox = [];
+  actionIndex = 1;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+    this.initActionBox();
+  }
+
+  initActionBox() {
+    for (this.actionIndex; this.actionIndex <= 10; this.actionIndex++) {
+      this.actionBox.push(`操作${this.actionIndex}`);
+    }
+  }
+
+  addAction() {
+    this.actionBox.push(`操作${this.actionIndex}`);
+    this.actionIndex++;
+  }
 }
