e4b7a1699d72fba6a2367de2bb6c26f39dbbcc41
LaamGinghong <lanjinghang@banggood.com>
2018-10-05 11:37:49 +0800

    完成personal组件

--- src/app/pages/content/widgets/widgets-personal/widgets-personal.component.ts
+++ src/app/pages/content/widgets/widgets-personal/widgets-personal.component.ts
@@ -6,11 +6,65 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./widgets-personal.component.scss']
 })
 export class WidgetsPersonalComponent implements OnInit {
+  option = {
+    xAxis: {
+      type: 'category',
+      show: false,
+      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+    },
+    yAxis: {
+      type: 'value',
+      show: false
+    },
+    tooltip: {
+      trigger: 'axis',
+      axisPointer: {
+        type: 'shadow'
+      }
+    },
+    series: [{
+      data: [120, 200, 150, 80, 70, 110, 130],
+      type: 'bar',
+      barWidth: 5,
+      itemStyle: {
+        color: '#A8A8A8'
+      }
+    }]
+  };
+  name: string;
+  data = [{
+    name: 'Recent Activity',
+    number: 350,
+    color: 'blue'
+  }, {
+    name: 'Following',
+    number: 150,
+    color: 'red'
+  }, {
+    name: 'Photos',
+    number: 100,
+    color: 'green'
+  }, {
+    name: 'Article',
+    number: 100,
+    color: 'purple'
+  }];
 
   constructor() {
   }
 
   ngOnInit() {
+    this.initName();
   }
 
+  initName(): void {
+    if (document.cookie.indexOf('username') >= 0) {
+      const cookies = document.cookie.split(';');
+      cookies.forEach((item: string) => {
+        if (item.indexOf('username') >= 0) {
+          this.name = item.split('=')[1];
+        }
+      });
+    }
+  }
 }
