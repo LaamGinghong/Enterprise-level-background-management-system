ee6b7bdd4ef2f7057352b211904de10077006366
LaamGinghong <351390560@qq.com>
2018-11-19 15:28:32 +0800

    修改后端逻辑

--- back/app.js
+++ back/app.js
@@ -1,13 +1,13 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var menu = require('./routes/menu');
-var dashboardCardLeft = require('./routes/dashboard/dashboardCardLeft');
+const menu = require('./routes/menu');
+const dashboardCard = require('./routes/dashboard/dashboard-card');
 
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/menu', menu);
-app.use('/dashboardCardLeft', dashboardCardLeft);
+app.use('/dashboardCard', dashboardCard)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- back/routes/dashboard/dashboard-card.js
+++ back/routes/dashboard/dashboard-card.js
@@ -5,9 +5,36 @@ const {query} = require('../../query');
 router.get('/', (request, response, next) => {
     let message;
     const addSql = 'update dashboard_card set number = case id when 1 then ? when 2 then ? when 3 then ? when 4 then ? END where id in (1,2,3,4)';
-    const number1 = Math.round(Math.random() * 100000);
-    const number2 = Math.round(Math.random() * 100000);
-    const number3 = Math.round(Math.random() * 100000);
-    const number4 = Math.round(Math.random() * 100000);
-
+    const value = [];
+    for (let i = 0; i < 4; i++) {
+        let number = '';
+        for (j = 0; j < 10; j++) {
+            number += j === 9 ? `${Math.round(Math.random() * 100000)}` : `${Math.round(Math.random() * 100000)},`
+        }
+        value.push(number);
+    }
+    query(addSql, value).then(value => {
+        const searchSql = 'SELECT * FROM dashboard_card;';
+        query(searchSql).then(val => {
+            message = {
+                success: true,
+                message: val
+            }
+        }).catch(error => {
+            throw new Error({
+                success: false,
+                message: error
+            })
+        }).finally(() => {
+            response.setHeader('Access-Control-Allow-Origin', '*');
+            response.send(JSON.stringify(message));
+        })
+    }).catch(error => {
+        throw new Error({
+            success: false,
+            message: error
+        })
+    })
 });
+
+module.exports = router;
