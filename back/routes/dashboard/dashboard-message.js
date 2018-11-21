616eeff72e5713d03d7be90d5943683b3c73e064
LaamGinghong <351390560@qq.com>
2018-11-21 15:19:04 +0800

    后端接口新增查询前重置删除

--- back/routes/dashboard/dashboard-message.js
+++ back/routes/dashboard/dashboard-message.js
@@ -4,20 +4,28 @@ const {query} = require('../../query');
 
 router.get('/', (request, response, next) => {
     let message;
-    const selectSql = 'select * from dashboard_message';
-    query(selectSql).then(value => {
-        message = {
-            success: true,
-            message: value
-        };
+    const updateSql = 'update dashboard_message set isDelete = 0';
+    query(updateSql).then(() => {
+        const selectSql = 'select * from dashboard_message';
+        query(selectSql).then(value => {
+            message = {
+                success: true,
+                message: value
+            };
+        }).catch(error => {
+            throw new Error({
+                success: false,
+                message: error
+            });
+        }).finally(() => {
+            response.setHeader('Access-Control-Allow-Origin', '*');
+            response.send(JSON.stringify(message));
+        })
     }).catch(error => {
         throw new Error({
             success: false,
             message: error
         });
-    }).finally(() => {
-        response.setHeader('Access-Control-Allow-Origin', '*');
-        response.send(JSON.stringify(message));
     })
 });
 
